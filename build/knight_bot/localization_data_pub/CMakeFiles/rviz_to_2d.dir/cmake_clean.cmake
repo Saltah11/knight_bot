3ac9426098e94976eef334462c88d730a595d335
Saltah11 <saltah1177@gmail.com>
2022-04-21 21:16:54 +0000

    commit

---
+++ build/knight_bot/localization_data_pub/CMakeFiles/rviz_to_2d.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/pi/catkin_ws/devel/lib/localization_data_pub/rviz_to_2d"
+  "/home/pi/catkin_ws/devel/lib/localization_data_pub/rviz_to_2d.pdb"
+  "CMakeFiles/rviz_to_2d.dir/src/rviz_to_2d.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rviz_to_2d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/knight_bot/navstack_pub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/catkin_ws/src/knight_bot/navstack_pub
+# Build directory: /home/pi/catkin_ws/build/knight_bot/navstack_pub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
